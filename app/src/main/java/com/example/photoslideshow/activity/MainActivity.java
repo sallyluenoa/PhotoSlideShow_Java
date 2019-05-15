3fca8d717380101b0876a26e6f683bfcc7f54135
SallyLueNoa <sally.lue.noa@gmail.com>
2019-05-15 23:38:58 +0900

    Implement giving necessary permissions by asking users.

--- app/src/main/java/com/example/photoslideshow/activity/MainActivity.java
+++ app/src/main/java/com/example/photoslideshow/activity/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.photoslideshow.activity;
 
+import android.Manifest;
 import android.accounts.Account;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -32,9 +37,12 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private static final String SCOPE_PHOTO_READONLY = "https://www.googleapis.com/auth/photoslibrary.readonly";
-    private static final int REQUEST_SIGN_IN = 1;
 
-    private static final int DIALOG_FAILED_SIGN_IN = 1;
+    private static final int ACV_REQ_CODE_SIGN_IN = 1;
+    private static final int ACV_REQ_CODE_PERMISSION = 2;
+
+    private static final int DLG_ID_FAILED_SIGN_IN = 1;
+    private static final int DLG_ID_FAILED_PERMISSION = 2;
 
     private GoogleApiClient mGoogleApiClient;
 
@@ -69,11 +77,10 @@ public class MainActivity extends AppCompatActivity
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                signIn();
+                givePermissions();
             }
         }, 2000);
     }
-
 
     @Override
     public void onStart() {
@@ -99,10 +106,10 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.d(TAG, "onActivityResult");
+        Log.d(TAG, "onActivityResult: " + requestCode);
 
         switch (requestCode) {
-            case REQUEST_SIGN_IN:
+            case ACV_REQ_CODE_SIGN_IN:
                 GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
 
                 if (result.isSuccess()) {
@@ -117,13 +124,35 @@ public class MainActivity extends AppCompatActivity
                     finish();
                 } else {
                     Log.d(TAG, "SignIn failed.");
-                    showFailedDialog();
+                    showSignInFailedDialog();
                 }
                 break;
             default:
                 break;
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: " + requestCode);
+
+        switch (requestCode) {
+            case ACV_REQ_CODE_PERMISSION:
+                boolean permission = true;
+                for (int i=0; i<grantResults.length; i++) {
+                    if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                        permission = false;
+                        break;
+                    }
+                }
+                if (permission) signIn();
+                else showPermissionsFailedDialog();
+                break;
+            default:
+                break;
+        }
+    }
+
 
     @Override
     public void onConnected(Bundle connectionHint) {
@@ -138,14 +167,17 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Log.d(TAG, "onConnectionFailed. errorMessage=" + connectionResult.getErrorMessage());
-        showFailedDialog();
+        showSignInFailedDialog();
     }
 
     @Override
     public void onPositiveClick(int id) {
         switch (id) {
-            case DIALOG_FAILED_SIGN_IN:
+            case DLG_ID_FAILED_SIGN_IN:
                 signIn();
+                break;
+            case DLG_ID_FAILED_PERMISSION:
+                givePermissions();
                 break;
             default:
                 break;
@@ -155,7 +187,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onNegativeClick(int id) {
         switch (id) {
-            case DIALOG_FAILED_SIGN_IN:
+            case DLG_ID_FAILED_SIGN_IN:
+            case DLG_ID_FAILED_PERMISSION:
                 finish();
                 break;
             default:
@@ -163,7 +196,28 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    public void signIn() {
+    private void givePermissions() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            Log.d(TAG, "No need to ask users. Skip giving permissions.");
+            signIn();
+            return;
+        }
+        if(isPermitted(Manifest.permission.READ_EXTERNAL_STORAGE) &&
+            isPermitted(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Log.d(TAG, "Already gave. Skip giving permissions.");
+            signIn();
+            return;
+        }
+        Log.d(TAG, "Need to ask users.");
+        ActivityCompat.requestPermissions(this,
+                new String[] {
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                },
+                ACV_REQ_CODE_PERMISSION);
+    }
+
+    private void signIn() {
         if (PreferenceUtils.hasAccountInfo(getApplicationContext())) {
             Log.d(TAG, "Already have account info. Skip sign in.");
             Intent slideShowIntent = new Intent(getApplicationContext(), SlideShowActivity.class);
@@ -179,15 +233,28 @@ public class MainActivity extends AppCompatActivity
         findViewById(R.id.progress_layout).setVisibility(View.VISIBLE);
 
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, REQUEST_SIGN_IN);
+        startActivityForResult(signInIntent, ACV_REQ_CODE_SIGN_IN);
     }
 
-    private void showFailedDialog() {
+    private void showPermissionsFailedDialog() {
+        DialogFragment fragment = ConfirmDialogFragment.newInstance(DLG_ID_FAILED_PERMISSION,
+                R.string.failed_permissions_dialog_title, R.string.failed_permissions_dialog_message,
+                R.string.retry, R.string.close);
+        fragment.show(getSupportFragmentManager(), TAG);
+    }
+
+    private void showSignInFailedDialog() {
         findViewById(R.id.progress_layout).setVisibility(View.GONE);
 
-        DialogFragment fragment = ConfirmDialogFragment.newInstance(DIALOG_FAILED_SIGN_IN,
+        DialogFragment fragment = ConfirmDialogFragment.newInstance
+                (DLG_ID_FAILED_SIGN_IN,
                 R.string.failed_sign_in_dialog_title, R.string.failed_sign_in_dialog_message,
                 R.string.retry, R.string.close);
         fragment.show(getSupportFragmentManager(), TAG);
+    }
+
+    private boolean isPermitted(String permission) {
+        return (ContextCompat.checkSelfPermission(getApplicationContext(), permission)
+                == PackageManager.PERMISSION_GRANTED);
     }
 }
